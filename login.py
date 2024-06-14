c6626cba988230473ccbc49a8600bae03f0c462e
Keigo <sliverkeigo343@gmail.com>
2024-06-14 14:29:00 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,50 +1,59 @@
 import sys
 from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-acccounts = int(len(sys.argv[1:])/2)
-print(f'Config {acccounts} accounts')
-for i in range(acccounts):
-    email = sys.argv[1+i]
-    passwd = sys.argv[1+i+acccounts]
+accounts = int(len(sys.argv[1:]) / 2)
+print(f'Config {accounts} accounts')
+for i in range(accounts):
+    email = sys.argv[1 + i]
+    passwd = sys.argv[1 + i + accounts]
     print('----------------------------')
 
-    #1.open browser
+    # 1. open browser
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(1000, 720)
     driver.get("https://game.maj-soul.net/1/")
-    print(f'Account {i+1} loading game...')
-    sleep(20)
-
-    #2.input email
-    screen = driver.find_element(By.ID, 'layaCanvas')
+    print(f'Account {i + 1} loading game...')
+    
+    # Adding WebDriverWait to wait for elements to load
+    wait = WebDriverWait(driver, 30)
+    
+    # Ensure the screen is loaded
+    screen = wait.until(EC.presence_of_element_located((By.ID, 'layaCanvas')))
+    
+    # 2. Input email
     ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, -100)\
+        .move_to_element_with_offset(screen, 1279.9, 278.031)\
         .click()\
         .perform()
-    driver.find_element(By.NAME, 'input').send_keys(email)
+    
+    email_input = wait.until(EC.presence_of_element_located((By.NAME, 'input')))
+    email_input.send_keys(email)
     print('Input email successfully')
 
-    #3.input password
+    # 3. Input password
     ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, -50)\
+        .move_to_element_with_offset(screen, 1279.9, 328.031)\
         .click()\
         .perform()
-    driver.find_element(By.NAME, 'input_password').send_keys(passwd)
+    
+    password_input = wait.until(EC.presence_of_element_located((By.NAME, 'input')))
+    password_input.send_keys(passwd)
     print('Input password successfully')
 
-    #4.login
+    # 4. Login
     ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, 50)\
+        .move_to_element_with_offset(screen, 1279.9, 378.031)\
         .click()\
         .perform()
+    
     print('Entering game...')
-    sleep(20) #loading...
+    sleep(20)  # waiting for the game to load...
     print('Login success')
     driver.quit()
