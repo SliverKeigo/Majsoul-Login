b862f361cfc2a66a53d3ef9a631962a0afc0754e
Keigo <sliverkeigo343@gmail.com>
2024-06-14 14:34:10 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,49 +1,50 @@
 import sys
 from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-accounts = int(len(sys.argv[1:]) / 2)
-print(f'Config {accounts} accounts')
-for i in range(accounts):
-    email = sys.argv[1 + i]
-    passwd = sys.argv[1 + i + accounts]
+
+acccounts = int(len(sys.argv[1:])/2)
+print(f'Config {acccounts} accounts')
+for i in range(acccounts):
+    email = sys.argv[1+i]
+    passwd = sys.argv[1+i+acccounts]
     print('----------------------------')
 
-    # 1. open browser
+    #1.open browser
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(1000, 720)
     driver.get("https://game.maj-soul.net/1/")
-    print(f'Account {i + 1} loading game...')
-    
-    # Adding WebDriverWait to wait for elements to load
-    wait = WebDriverWait(driver, 30)
-    
-    # Ensure the screen is loaded
-    screen = wait.until(EC.presence_of_element_located((By.ID, 'layaCanvas')))
+    print(f'Account {i+1} loading game...')
+    sleep(20)
 
-    # 2. Input email
-    email_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="input"]')))
-    email_input.click()
-    email_input.send_keys(email)
+    #2.input email
+    screen = driver.find_element(By.ID, 'layaCanvas')
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, -100)\
+        .click()\
+        .perform()
+    driver.find_element(By.NAME, 'input').send_keys(email)
     print('Input email successfully')
 
-    # 3. Input password
-    password_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="input_password"]')))
-    password_input.click()
-    password_input.send_keys(passwd)
+    #3.input password
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, -50)\
+        .click()\
+        .perform()
+    driver.find_element(By.NAME, 'input_password').send_keys(passwd)
     print('Input password successfully')
 
-    # 4. login
-    login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '目标按钮的CSS选择器'))) # 请替换为实际按钮的选择器
-    login_button.click()
-    
+    #4.login
+    ActionChains(driver)\
+        .move_to_element_with_offset(screen, 250, 50)\
+        .click()\
+        .perform()
     print('Entering game...')
-    sleep(20)  # waiting for the game to load...
+    sleep(20) #loading...
     print('Login success')
     driver.quit()
